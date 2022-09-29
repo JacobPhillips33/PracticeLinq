c7f6d383547b00b67a88b2df9a228f73d01af130
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-29 11:34:59 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,8 @@
+﻿var videoGames = new List<string>() { "mario", "mario cart", "donkey kong country", "final fantasy", "legend of zelda"};
+
+var orderedGamesByLength = videoGames.OrderBy(x => x.Length);
+
+foreach (var game in orderedGamesByLength)
+{
+    Console.WriteLine(game);
+}
